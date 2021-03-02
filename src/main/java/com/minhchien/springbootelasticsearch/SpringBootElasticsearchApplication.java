e830e79d04df9b61eda4550861d9c87b235cfbe9
Chien DUONG <chienduong90@gmail.com>
2021-03-02 11:57:12 +0100

    spring boot elasticsearch demo

---
+++ src/main/java/com/minhchien/springbootelasticsearch/SpringBootElasticsearchApplication.java
@@ -0,0 +1,15 @@
+package com.minhchien.springbootelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SpringBootElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootElasticsearchApplication.class, args);
+	}
+
+}
